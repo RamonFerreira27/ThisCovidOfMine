56f2bf5c62fc0313a060bbf662314eb554448020
RamonGregorio <ramon_ferreira27@hotmail.com>
2020-08-22 05:48:30 -0300

    Basic status bar and action menu button scripts

---
+++ Assets/Scripts/InterfaceScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceScript : MonoBehaviour
+{
+    public Slider StressSlider;
+    public Slider LifeSlider;
+    public Slider RestSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StressSlider.value--;
+    }
+}
---
+++ Assets/Scripts/MenuButtonsScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButtonsScript : MonoBehaviour
+{
+    public Button ActionsButton;
+    public GameObject ActionsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateMenu()
+    {
+        ActionsMenu.GetComponent<GameObject>();
+        if (ActionsMenu.activeSelf)
+            ActionsMenu.SetActive(false);
+        else
+            ActionsMenu.SetActive(true);
+    }
+}
