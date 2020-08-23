71f4434846756bae30419e07bfd3feca9075baef
RamonGregorio <ramon_ferreira27@hotmail.com>
2020-08-22 21:19:51 -0300

    Não podem dizer que não tentamos!

--- Assets/Scripts/MenuButtonsScript.cs
+++ Assets/Scripts/MenuButtonsScript.cs
@@ -5,20 +5,7 @@ using UnityEngine.UI;
 
 public class MenuButtonsScript : MonoBehaviour
 {
-    public Button ActionsButton;
     public GameObject ActionsMenu;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ActivateMenu()
     {
@@ -28,4 +15,6 @@ public class MenuButtonsScript : MonoBehaviour
         else
             ActionsMenu.SetActive(true);
     }
+
+    
 }
